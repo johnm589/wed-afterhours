c56d94dfcf59bac3a2f0f931007206cd36f95440
david szyszkowski <try2murkme@gmail.com>
2016-03-09 19:20:53 -0800

    added my loop, bruh

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ var myArr = ["john","jeff","conrad","jill","coolguy"]
 for (var i = 0; i < myArr.length; ++i) {
   console.log(myArr[i]);
 }
+
+
+var instructors = ["phil", "matt", "conrad", "kate", "john", "super dave"];
+var index = 0;
+
+while (index < instructors.length) {
+
+  console.log(instructors[index]);
+  index ++;
+ }
